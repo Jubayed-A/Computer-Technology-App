37770ad33b60d36e084206b21f87b04e8237c466
Jubayed-A <learnigprograming@gmail.com>
2023-12-07 02:06:29 +0600

    some ui design added

---
+++ app/src/main/java/com/example/computertecnology/StudentInformation.kt
@@ -0,0 +1,11 @@
+package com.example.computertecnology
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_information)
+    }
+}
