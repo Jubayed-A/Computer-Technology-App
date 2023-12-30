a4b24698f8c64cfd200a309b903cd1b5f138ea70
Jubayed Alam <106887389+Jubayed-A@users.noreply.github.com>
2023-12-30 10:22:43 +0600

    Update NoticeDetails.kt

--- app/src/main/java/com/example/computertecnology/notice/NoticeDetails.kt
+++ app/src/main/java/com/example/computertecnology/notice/NoticeDetails.kt
@@ -11,14 +11,17 @@ class NoticeDetails : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notice_details)
 
+// intent gets 
         val name = intent.getStringExtra("name")
         val description = intent.getStringExtra("description")
         val image = intent.getIntExtra("image", R.drawable.img2)
 
+// variable or I'd declaration 
         val noticeName = findViewById<TextView>(R.id.noticeName)
         val noticeDescription = findViewById<TextView>(R.id.noticeDescription)
         val noticeImage = findViewById<ImageView>(R.id.noticeImg)
 
+// intent set to the variable 
         noticeName.text = name
         noticeDescription.text = description
         noticeImage.setImageResource(image)
